356716099525732ef980c5d20c9f51f6c297701d
4lac1vica <hp922050@gmail.com>
2026-03-06 16:09:22 +0200

    student + profesor

--- AplicatieCatalog/Models/Profesor.cs
+++ AplicatieCatalog/Models/Profesor.cs
@@ -1,7 +1,28 @@
-﻿namespace AplicatieCatalog.Models
+﻿using System.Runtime.CompilerServices;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace AplicatieCatalog.Models
 {
     public class Profesor
     {
+       public int ID { get; set; }
+       public string lastName { get; set; }
        
+       public string firstName { get; set; }
+       
+       public string telefon { get; set; }
+        
+       
+       public string email { get; set; }
+       
+       public string materie { get; set; }
+       
+       public string adresa { get; set; }
+       
+       public string parolaHash { get; set; }
+       
+       public int varsta { get; set; }
+       
+        
     }
 }
--- AplicatieCatalog/Models/Student.cs
+++ AplicatieCatalog/Models/Student.cs
@@ -2,6 +2,23 @@
 {
     public class Student
     {
-         
+        public int ID { get; set; }
+        public string lastName { get; set; }
+        public string firstName { get; set; }
+
+        public string email { get; set; }
+        public string parolaHash { get; set; }
+
+        public int numarGrupa { get; set; }
+
+        public string specializare { get; set; }
+
+        public string telefon { get; set; }
+
+        public string adresaOrigine { get; set; }
+
+        public string adresaCurenta { get; set; }
+
+        public int varsta { get; set; }
     }
 }
